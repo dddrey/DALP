e833c7afd982e6949cced894f7a76eaabdd2199f
dddrey <drachyov.andrew@gmail.com>
2020-09-24 13:48:31 +0400

    user created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_09_24_071527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "middle_name"
+    t.date "birth_date"
+    t.string "city"
+    t.string "education"
+    t.string "arab_study"
+    t.boolean "movement_help", default: false
+    t.string "phone", default: "", null: false
+    t.text "social_links"
+    t.string "competition_source"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["phone"], name: "index_users_on_phone", unique: true
+  end
 
 end
