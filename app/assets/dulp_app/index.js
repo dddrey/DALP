0782121bf22d1d6be63561fa2104aaf7c750148e
panddr <panddr@gmail.com>
2020-09-28 18:55:50 +0300

    arab landing

--- app/assets/dulp_app/index.js
+++ app/assets/dulp_app/index.js
@@ -1,5 +1,6 @@
 import './stylesheets/index.scss';
 import { scrollIt } from "./scroll_it";
+import "./remove_hover_on_touch";
 window.scrollIt = scrollIt;
 
 document.addEventListener('DOMContentLoaded', () => {
