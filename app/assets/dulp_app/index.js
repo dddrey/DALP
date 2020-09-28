ee467e3b2d56571927a8709e783df16606295373
panddr <panddr@gmail.com>
2020-09-28 16:41:11 +0300

    video for ios

--- app/assets/dulp_app/index.js
+++ app/assets/dulp_app/index.js
@@ -1,4 +1,6 @@
 import './stylesheets/index.scss';
+import { scrollIt } from "./methods/scroll_it";
+window.scrollIt = scrollIt;
 
 document.addEventListener('DOMContentLoaded', () => {
   // console.log('DOMContentLoaded');
