cafc1ae2c4f087172de7583734c6c1da07711ce9
dddrey <drachyov.andrew@gmail.com>
2020-09-28 22:18:33 +0400

    locale set fix

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,14 @@ module DALP
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.i18n.fallbacks = true
+    config.time_zone = 'Moscow'
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+    config.i18n.available_locales = %i(ru en)
     config.i18n.default_locale = :ru
+    config.i18n.fallbacks = %i(en)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
