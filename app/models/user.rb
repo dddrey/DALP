0973afc603e7de7673a521eac6bb479db410226d
dddrey <drachyov.andrew@gmail.com>
2020-09-29 14:36:33 +0400

    user validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,17 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: Devise.email_regexp }
   # validates :phone, presence: true, uniqueness: true, format: { with: PHONE_NUMBER_REGEXP }
 
+  validate :full_name_uniqueness, if: proc { |user| user.middle_name.present? }
+
+  before_save :set_movement_help, if: proc { |user| user.movement_help == nil }
+
+  private
+
+  def full_name_uniqueness
+    errors.add(:middle_name, 'ФИО уже зарегистрирован в системе') if User.where('last_name ILIKE ? AND first_name ILIKE ? AND middle_name ILIKE ?', last_name, first_name, middle_name).any?
+  end
+
+  def set_movement_help
+    self.movement_help = false
+  end
 end
