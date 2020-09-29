88fe78b6e3c2e790e2e4d5e58e4c3739d2e2d73e
dddrey <drachyov.andrew@gmail.com>
2020-09-29 15:06:56 +0400

    remove trash

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -7,21 +7,15 @@ class Admin::QuestionsController < AdminController
     @question = Question.find(params[:id])
 
     right_answers = question_params[:answers_attributes].keys.filter{|key| question_params[:answers_attributes][key]['right'] == '1'}
-    p right_answers
-
-    # @question.errors.add(:answers, 'Only one right answer is allowed') if right_answers.length != 1
 
     @question.assign_attributes(question_params)
     @question.right_answers_count = right_answers.length
 
-    # if @question.update(question_params.merge!(right_answers_count: right_answers.length))
     if @question.save
       flash[:notice] = 'Updated'
       redirect_to edit_admin_question_path(@question)
-      # render :edit
     else
       flash[:error] = 'Oops. Check errors'
-      # redirect_to edit_admin_question_path(@question)
       render :edit
     end
   end
