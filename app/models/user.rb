c24ad31d481ff11caa44d729bf19f122d2f54af7
dddrey <drachyov.andrew@gmail.com>
2020-09-29 14:35:37 +0400

    remember admin & user for 6 months

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
   PHONE_NUMBER_REGEXP = /\A7[\d]{10}\z/
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+  # devise :registerable, :rememberable, :validatable
+  devise :registerable, :rememberable, remember_for: 6.months
 
   validates_presence_of :last_name, :first_name, :birth_date, :city, :education
 
+  validates :phone, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: Devise.email_regexp }
-  validates :phone, presence: true, uniqueness: true, format: { with: PHONE_NUMBER_REGEXP }
+  # validates :phone, presence: true, uniqueness: true, format: { with: PHONE_NUMBER_REGEXP }
+
 end
