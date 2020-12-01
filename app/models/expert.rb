6c70619386ae1fb136656b3edb1f07f03e66f4c1
dddrey <drachyov.andrew@gmail.com>
2020-12-01 12:17:22 +0400

    Update expert.rb

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -1,5 +1,5 @@
 class Expert < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :rememberable, :validatable
+
+  has_many :interviews
 end
