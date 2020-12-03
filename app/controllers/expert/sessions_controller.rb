c3d7d552e8e689c4dc20b98f45ebe61b4e2ff32d
dddrey <drachyov.andrew@gmail.com>
2020-12-03 11:45:42 +0400

    Update sessions_controller.rb

--- app/controllers/expert/sessions_controller.rb
+++ app/controllers/expert/sessions_controller.rb
@@ -2,11 +2,19 @@
 
 class Expert::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  # prepend_before_action :require_no_authentication, only: [:new, :create]
+  prepend_before_action :require_no_authentication, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+
+  def new
+    return redirect_to '/expert/dashboard' if current_expert.present?
+
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
+  end
 
   # POST /resource/sign_in
   def create
