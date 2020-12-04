ceceb3a44512f616b123e673a59ca5ef31a43cbe
dddrey <drachyov.andrew@gmail.com>
2020-12-04 20:45:05 +0400

    push spec

---
+++ spec/mailers/previews/second_stage_preview.rb
@@ -0,0 +1,13 @@
+# Preview all emails at http://localhost:3000/rails/mailers/second_stage
+class SecondStagePreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/second_stage/email
+  def email
+    SecondStageMailer.email(13)
+  end
+
+  def thanks_email
+    SecondStageMailer.thanks_email(454)
+  end
+
+end
